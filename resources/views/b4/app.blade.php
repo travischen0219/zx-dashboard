62a64f6dd7b870a65535131c339e155bd209043d
Wayne Yen <wayway1974@gmail.com>
2019-06-15 13:59:42 +0800

    APP.CSS

--- resources/views/b4/app.blade.php
+++ resources/views/b4/app.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title') | 真心蓮坊進銷存系統</title>
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="/css/app.css?1">
     @yield('css')
 </head>
 <body>
